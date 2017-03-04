2cc2333cfb34b48751e3d894cf39d7ce028f2c11
jjron <jaren.escueta@gmail.com>
2017-03-03 20:21:45 -0800

    use mongodb instead of firebase

---
+++ server/test/lib/mock-env.js
@@ -0,0 +1,10 @@
+'use strict';
+
+process.env.PORT=3000;
+process.env.MONGODB_URI='mongodb://localhost/test';
+process.env.APP_SECRET='thomasStillUsesTooManyEmojis';
+process.env.AWS_BUCKET='thiscanbeanything';
+process.env.AWS_ACCESS_KEY_ID='thisalsocanbeanything';
+process.env.AWS_SECRET_ACCESS_KEY='thisshouldbecool';
+
+require('./lib/aws-mocks.js');
