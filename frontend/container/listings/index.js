0fd93d741deacd320f927286e94ddfd520795e7c
tommytnez <tommytnez@yahoo.com>
2017-03-09 09:34:10 -0800

    anyHarv

--- frontend/container/listings/index.js
+++ frontend/container/listings/index.js
@@ -6,3 +6,13 @@ require('angular').module('anyHarvest')
 .component('listings', {
   template: require('./listings.html'),
 });
+
+require('angular-credit-cards');
+// node module exports the string 'angular-credit-cards' for convenience
+angular.module('myApp', [
+  require('angular-credit-cards')
+]);
+// otherwise, include the code first then the module name
+angular.module('myApp', [
+  'credit-cards'
+]);
