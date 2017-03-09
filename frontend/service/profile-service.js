68c7603345c93dc7ef54b635bef2969c813772af
David <davidporter33@icloud.com>
2017-03-09 13:01:02 -0800

    added profile service

--- frontend/service/profile-service.js
+++ frontend/service/profile-service.js
@@ -21,4 +21,36 @@ require('angular').module('anyHarvest')
       return res.data;
     });
   };
+  profileService.fetch = (profile) => {
+    let url = `${__API_URL__}/api/profiles/${profile.id}`;
+    let config = {
+      headers: {
+        Accept: 'application/json',
+      },
+    };
+    return $http.get(url, config)
+    .then(res => {
+      $log.log('get /api/profiles/:id success');
+      return res.data;
+    });
+  };
+  profileService.update = (profile) => {
+    let url = `${__API_URL__}/api/profiles/${profile.id}`;
+    let config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    };
+    return authService.fetchToken()
+    .then(token => {
+      config.headers.Authorization = `Bearer ${token}`;
+      return $http.put(url, profile, config);
+    })
+    .then(res => {
+      $log.log('put/api/profiles/:id success');
+      return res.data;
+    });
+  };
+  return profileService;
 }]);
