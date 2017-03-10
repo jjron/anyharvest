bd600726f1f10f8b75e12bb4f0347312c7dce10d
jjron <jaren.escueta@gmail.com>
2017-03-10 11:14:35 -0800

    show listings works

--- frontend/container/home/index.js
+++ frontend/container/home/index.js
@@ -8,13 +8,17 @@ require('angular').module('anyHarvest')
   controller:['$log', 'listingService', function($log, listingService){
     this.$onInit = () => {
       this.test = 'BOOYA';
+
+      console.log('hello')
       listingService.fetchAll()
       .then(listings => {
+        console.log('listings', listings  )
         this.listings = listings;
         this.selected = this.listings[0];
         $log.log('selectedListing', this.selected.product);
       })
       .catch($log.error);
+
       this.itemHandleSelect = listing => {
         this.selected = listing;
       };
